8e6aa06923e4281c6acf73e942f5ffe31e476301
Joe Schrag <gh@joeschrag.com>
2014-01-17 16:23:13 -0700

    Improved formatting & added binary
    values showing up in all boxes after calculation.  Need to test.

--- scripts/DataTypeConverter.js
+++ scripts/DataTypeConverter.js
@@ -1,5 +1,7 @@
-var register1Box;
-var register2Box;
+var firstShortBox;
+var secondShortBox;
+var firstBinaryBox;
+var secondBinaryBox;
 var hexValueBox;
 var floatValueBox;
 var I4ValueBox;
@@ -7,22 +9,41 @@ var UI4ValueBox;
 
 window.onload = function()
 {
-	register1Box = document.getElementById('binaryRegister1');
-	register2Box = document.getElementById('binaryRegister2');
+	firstShortBox = document.getElementById('firstShort');
+	secondShortBox = document.getElementById('secondShort');
+	firstBinaryBox = document.getElementById('firstBinary');
+	secondBinaryBox = document.getElementById('secondBinary');
 	hexValueBox = document.getElementById('hexValue');
 	floatValueBox = document.getElementById('floatValue');
 	I4ValueBox = document.getElementById('I4Value');
 	UI4ValueBox = document.getElementById('UI4Value');
 }
 
+function SwapRegisters()
+{
+	alert("Register swap not yet implemented");
+}
+
 //incoming values expected to be signed 16 bit integers
 function RegistersChanged()
 {
-	var temp = parseInt(register1Box.value);
+	var temp = parseInt(firstShortBox.value);
 	var intArray = new Int16Array(2);
 	intArray[0] = temp;
-	
-	temp = parseInt(register2Box.value);
+
+	temp = parseInt(secondShortBox.value);
+	intArray[1] = temp;
+
+	SetValues(intArray.buffer);
+}
+
+function BinaryChanged()
+{
+	var intArray = new Int16Array(2);
+	var temp = parseInt(firstBinaryBox.value, 2);
+	intArray[0] = temp;
+
+	temp = parseInt(secondBinaryBox.value, 2);
 	intArray[1] = temp;
 
 	SetValues(intArray.buffer);
@@ -55,52 +76,26 @@ function I4Changed()
 function UI4Changed()
 {
 	var uIntArray = new Uint32Array(1);
-	uIntArray[0] = parseInt(I4ValueBox.value);
+	uIntArray[0] = parseInt(UI4ValueBox.value);
 	SetValues(uIntArray.buffer);
 }
 
 function SetValues(rawValueBuffer)
 {
-	var floatArray = new Int32Array(rawValueBuffer);
+	var floatArray = new Float32Array(rawValueBuffer);
 	floatValueBox.value = floatArray[0].toString();
 	
-	var intArray = new Int32Array(floatArray.buffer);
+	var intArray = new Int32Array(rawValueBuffer);
 	I4ValueBox.value = intArray[0].toString();
 	
 	var uIntArray = new Uint32Array(rawValueBuffer);
 	UI4ValueBox.value = uIntArray[0].toString();
 	
 	hexValueBox.value = intArray[0].toString(16);
-}
-
-//This needs to be rewritten if I actually take binary input
-function BinaryChanged()
-{
-	var numberOfPlaces = 16;
-
-	var zero = "";
-	for (var i = 0; i < numberOfPlaces; i++)
-	{
-		zero += "0";
-	}
 	
-	var binaryString1 = zero + binaryRegister1Box.value;
-	var binaryString2 = zero + binaryRegister2Box.value;
-	binaryString1 = binaryString1.slice(-16);
-	binaryString2 = binaryString2.slice(-16);
-	var binaryString = binaryString1 + binaryString2;
-alert(binaryString);
-	var result = 0;
-	
-	for (var i = 0; i < numberOfPlaces; i++)
-	{
-		var character = binaryString.slice(i, i + 1);
-		var bit = parseInt(character);
-		result += bit*2^i;
-	}
-	
-	
-	var intArray = new Int32Array(1);
-	intArray[0] = result;
-	SetValues(intArray.buffer);
+	var shortArray = new Int16Array(rawValueBuffer, 0, 2);
+	firstShortBox.value = shortArray[0];
+	secondShortBox.value = shortArray[1];
+	firstBinaryBox.value = shortArray[0].toString(2);
+	secondBinaryBox.value = shortArray[1].toString(2);
 }
