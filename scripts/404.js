42099457cb66cc8113f2e0d57ad9cc76e32ff10c
Joe Schrag <gh@joeschrag.com>
2014-02-01 00:20:49 -0700

    Switched approach to simple 404 page with links

--- scripts/404.js
+++
@@ -1,8 +0,0 @@
-if (document.location.href.indexOf("DataTypeConverter") !== -1)
-{
-	window.location.href = "http://jlschrag.github.io/DataTypeConverter";
-}
-else
-{
-	window.location.href = "http://josephschrag.com";
-}
