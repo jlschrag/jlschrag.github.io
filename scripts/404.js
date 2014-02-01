9e3e8e4987b7a625f7c5f23443d24e32ee36aa54
Joe Schrag <gh@joeschrag.com>
2014-01-31 23:54:01 -0700

    another attempt
    redirects still not working

--- scripts/404.js
+++ scripts/404.js
@@ -1,8 +1,8 @@
 if (document.URL.indexOf("DataTypeConverter") !== -1)
 {
-	window.location = "http://jlschrag.github.io/DataTypeConverter"
+	window.location.replace("http://jlschrag.github.io/DataTypeConverter");
 }
 else
 {
-	window.location = "http://josephschrag.com"
+	window.location.replace("http://josephschrag.com");
 }
