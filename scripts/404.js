9ea4ccf5558f7f537faf1aad9d312e2bf3ab6949
Joe Schrag <gh@joeschrag.com>
2014-01-31 23:42:20 -0700

    Playing with custom redirections

---
+++ scripts/404.js
@@ -0,0 +1,8 @@
+if (document.URL === "http://jlschrag.github.io/DataTypeConverter.html")
+{
+	window.location.replace('jlschrag.github.io/DataTypeConverter');
+}
+else
+{
+	window.location.replace('josephschrag.com');
+}
